b93cbaee058846581ab0d59ef0d89a17ae3b0b7b
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-15 12:29:54 +0530

    winston logs functions

---
+++ utils/logger.js
@@ -0,0 +1,24 @@
+import winston from "winston";
+
+// Configure the logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
+
+
+
+export { logger }
