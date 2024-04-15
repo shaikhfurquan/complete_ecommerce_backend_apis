4d2e0f0a77e1c8c6a05c6c7d2b5f08f93fd04700
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-15 15:05:18 +0530

    role added

--- models/userModel.js
+++ models/userModel.js
@@ -6,13 +6,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'First name is required.'],
         minlength: [2, 'First name must be at least 2 characters long.'],
-        maxlength: [50, 'First name cannot exceed 50 characters.']
+        maxlength: [50, 'First name cannot exceed 50 characters.'],
+        trim: true,
     },
     lastName: {
         type: String,
         required: [true, 'Last name is required.'],
         minlength: [2, 'Last name must be at least 2 characters long.'],
-        maxlength: [50, 'Last name cannot exceed 50 characters.']
+        maxlength: [50, 'Last name cannot exceed 50 characters.'],
+        trim: true,
     },
     email: {
         type: String,
@@ -23,27 +25,35 @@ const userSchema = new mongoose.Schema({
                 return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
             },
             message: 'Please enter a valid email address.'
-        }
+        },
+        trim: true,
     },
     mobile: {
         type: String,
         required: [true, 'Mobile number is required'],
-        // unique: [true, 'Mobile number must be unique']
+        // unique: [true, 'Mobile number must be unique'],
+        trim: true,
     },
     password: {
         type: String,
         required: [true, 'Password is required.'],
-        minlength: [3, 'Password must be at least 3 characters long.']
+        minlength: [3, 'Password must be at least 3 characters long.'],
+        trim: true,
+    },
+    role : {
+        type : String,
+        enum : ['admin', 'user'] ,
+        default : "user"
     }
-});
+} , {timestamps : true});
 
 
-userSchema.pre('save' , async function(){
-    this.password = await bcrypt.hash(this.password , 10)
+userSchema.pre('save', async function () {
+    this.password = await bcrypt.hash(this.password, 10)
 })
 
-userSchema.methods.isPasswordMatch = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword , this.password)
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
 }
 
 const UserModel = mongoose.model('User', userSchema);
