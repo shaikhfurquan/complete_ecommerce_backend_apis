5df155eb1aac3f11e2f9bce54e606c655b5724b4
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-15 15:43:58 +0530

    get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,13 +22,15 @@ export const registerUser = async (req, res) => {
         const newUser = await UserModel.create(req.body)
         // logger.info('User registered successfully');
 
-        // ApiSuccessResponse(res, 'User register successfully', newUser, 201)
+        // removing the password from the response
+        newUser.password = undefined
         res.status(201).json({
             success : true,
             message : 'User registered successfully',
-            user : newUser,
+            newUser : newUser,
         })
-
+        
+        // ApiSuccessResponse(res, 'User register successfully', newUser, 201)
 
     } catch (error) {
         // logger.error('Error while registering user', error.message , {error});
@@ -41,21 +43,21 @@ export const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body
         if (!email || !password) {
-            logger.error('Email and password is required for login')
+            // logger.error('Email and password is required for login')
             return ApiValidationResponse(res, 'Email and password is required for login', 404)
         }
 
         // check user exists or not
         const findUser = await UserModel.findOne({ email })
         if (!findUser) {
-            logger.error('User not found, register first')
+            // logger.error('User not found, register first')
             return ApiValidationResponse(res, 'User not found, register first', 404)
         }
 
         // compare given password with the hashed
         const isPasswordMatch = await findUser.isPasswordMatched(password)
         if(!isPasswordMatch){
-            logger.error('Password mismatch')
+            // logger.error('Password mismatch')
             return ApiValidationResponse(res, 'Invalid credentials' , 404)
         }
 
@@ -65,17 +67,36 @@ export const loginUser = async (req, res) => {
         const token = generateToken(findUser)
         // console.log(token);
 
-        logger.info('User login successfully')
-        // ApiSuccessResponse(res, `Welcome ${findUser.firstName}` , findUser, 200)
+        // logger.info('User login successfully')
         res.status(200).json({
             success : true,
             message : `Welcome ${findUser.firstName}`,
             user : findUser,
             token
         })
-
+        // ApiSuccessResponse(res, `Welcome ${findUser.firstName}` , findUser, 200)
+        
     } catch (error) {
-        logger.error('Error while login user', error.message, { error });
+        // logger.error('Error while login user', error.message, { error });
         ApiCatchResponse(res, 'Error while login user', error.message)
+    }
+}
+
+
+export const getAllUsers = async (req, res) =>{
+    try {
+        const getAllUsers = await UserModel.find()
+        if(!getAllUsers){
+            return ApiValidationResponse(res , 'Users not found' , 404)
+        }
+
+        res.status(200).json({
+            success : true,
+            message : `Users fetch successfully`,
+            allUsersCount : getAllUsers.length,
+            allUsers : getAllUsers,
+        })
+    } catch (error) {
+        ApiCatchResponse(res, 'Error while getting users', error.message)        
     }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController.js';
+import { getAllUsers, loginUser, registerUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register' , registerUser)
 userRouter.post('/login' , loginUser)
+userRouter.get('/get-all' , getAllUsers)
 
 
 export default userRouter
