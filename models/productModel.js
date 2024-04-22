79e671a2de9112dcb40c1e2d89491628821a3073
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-22 13:49:20 +0530

    update product model

--- models/productModel.js
+++ models/productModel.js
@@ -2,13 +2,23 @@ import mongoose from 'mongoose';
 
 
 const productSchema = new mongoose.Schema({
-    name: {
+    title: {
         type: String,
-        required: [true, 'Name is required']
+        required: [true, 'Name is required'],
+        trim: true,
+        lowercase: true
+    },
+    slug: {
+        type: String,
+        required: [true, 'slug is required'],
+        trim: true,
+        lowercase: true
     },
     description: {
         type: String,
-        required: [true, 'Description is required']
+        required: [true, 'Description is required'],
+        trim: true,
+        lowercase: true
     },
     price: {
         type: Number,
@@ -16,24 +26,38 @@ const productSchema = new mongoose.Schema({
         min: [0, 'Price must be a positive number']
     },
     category: {
-        type: String,
-        required: [true, 'Category is required']
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'category'
     },
     brand: {
         type: String,
-        required: [true, 'Brand is required']
+        required: [true, 'Brand is required'],
+        enum: ['Apple', 'Samsung', 'Blackbery', 'Lenovo']
     },
     quantity: {
         type: Number,
-        required: [true, 'Quantity is required'],
-        min: [0, 'Quantity must be a positive number']
+        min: [0, 'Quantity must be a positive number'],
     },
-    imageUrl: {
+    sold: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array
+    },
+    color: {
         type: String,
-        required: [true, 'Image URL is required']
-    }
-} , {timestamps : true});
+        enum: ['Black', 'White', 'Brown', 'Red']
+    },
+    ratings: [{
+        star: Number,
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+    }]
+}, { timestamps: true });
 
 const ProductModel = mongoose.model('Product', productSchema);
 
-export  {ProductModel}
+export { ProductModel }
