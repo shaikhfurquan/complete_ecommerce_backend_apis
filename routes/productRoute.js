fe67ede9f195e4fc2be3f66388b8595d80ea1b15
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-26 23:25:04 +0530

    admin auth

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,19 +1,19 @@
 import express from 'express';
-import { isAuthenticated } from '../middlewares/isAuthMiddleware.js';
+import { isAdmin, isAuthenticated } from '../middlewares/isAuthMiddleware.js';
 import { createProduct, deleteProduct, getAllProducts, getaProduct, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 
-productRouter.post('/create' , isAuthenticated , createProduct)
+productRouter.post('/create', isAuthenticated, isAdmin, createProduct)
 
-productRouter.get('/get/:productId' , getaProduct)
+productRouter.get('/get/:productId', getaProduct)
 
-productRouter.get('/get-all' , getAllProducts)
+productRouter.get('/get-all', getAllProducts)
 
-productRouter.put('/update/:productId' , isAuthenticated ,  updateProduct)
+productRouter.put('/update/:productId', isAuthenticated, isAdmin, updateProduct)
 
-productRouter.delete('/delete/:productId' , isAuthenticated ,  deleteProduct)
+productRouter.delete('/delete/:productId', isAuthenticated, isAdmin, deleteProduct)
 
 
 export default productRouter
