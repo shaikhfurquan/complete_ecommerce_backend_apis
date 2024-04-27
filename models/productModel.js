c3065df49170d83a408e2baefea30c6a44e7b087
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-04-27 14:46:46 +0530

    update sold

--- models/productModel.js
+++ models/productModel.js
@@ -43,7 +43,8 @@ const productSchema = new mongoose.Schema({
     },
     sold: {
         type: Number,
-        default: 0
+        default: 0,
+        select : false,
     },
     images: {
         type: Array
